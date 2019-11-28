866fdc327cb5096d29fd8db4401b14e0005e4d17
DarionHernandez <darionhernandez868@gmail.com>
2019-11-27 23:36:13 -0400

    Created basic example to be invoked locally for testing

---
+++ examples/app.js
@@ -0,0 +1,35 @@
+const OpenApiValidator = require('src/index');
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+
+exports.lambdaHandler = new OpenApiValidator(
+    {
+        apiSpec: 'examples/swagger.json',
+        validateRequests: true,
+    },
+    async (event, context) => {
+        try {
+            const response = {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    message: 'hello world',
+                })
+            }
+            return response;
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+}).install();
